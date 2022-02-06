f72110c6a79a149306db1f91fbbe7d67157542fd
Alex Waldron <awaldro1@stevens.edu>
2022-02-06 14:47:51 -0500

    added bootstrap

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 
 const App = () => (
-    <html>
-        <body>
     <div>
         <h1>Contaxt Website</h1>
     </div>
-    </body>
-    </html>
+    
 )
 
 export default App;
