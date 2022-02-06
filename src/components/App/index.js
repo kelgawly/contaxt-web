d46a85fc15e9b63de92ce8eae180ae040499a7e5
Alex Waldron <awaldro1@stevens.edu>
2022-02-06 11:24:43 -0500

    completed basic react framework

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => (
+    <html>
+        <body>
+    <div>
+        <h1>Contaxt Website</h1>
+    </div>
+    </body>
+    </html>
+)
+
+export default App;
