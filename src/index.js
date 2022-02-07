5db84ef57d300c48d2d2804309237d4d22375942
Alex Waldron <awaldro1@stevens.edu>
2022-02-07 12:20:30 -0500

    removed unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
